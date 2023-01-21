9f8faa962c8cc33efbc284ddb52aee5485712dd7
Pascal-Osemene <pascalosemene@gmail.com>
2023-01-21 17:31:51 +0100

    I added borders to the input fields

--- resources/views/admin/baby-records/create.blade.php
+++ resources/views/admin/baby-records/create.blade.php
@@ -8,27 +8,27 @@
 
                     <div class="mb-3">
                         <label for="formGroupExampleInput" class="form-label">Name of Child</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name of child">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput" placeholder="Name of child">
                       </div>
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Parent's Name</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Parent's Name">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Parent's Name">
                       </div>
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Hospital Name</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Hospital Name">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Hospital Name">
                       </div>
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput" class="form-label">Doctor</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Doctor's Name">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput" placeholder="Doctor's Name">
                       </div>
 
                       <div class="mb-3">
                         <label class="visually-hidden" for="inlineFormSelectPref"></label>
-                      <select class="form-select" id="inlineFormSelectPref">
+                      <select class="form-select border border-success" id="inlineFormSelectPref">
                         <option selected>Gender</option>
                         <option value="1">Male</option>
                         <option value="2">Female</option>
@@ -40,29 +40,29 @@
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput" class="form-label">Weight</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Weight">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput" placeholder="Weight">
                       </div>
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Height</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Height">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Height">
                       </div>
 
 
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Date and Time of Delivery</label>
-                        <input type="datetime-local" class="form-control" id="formGroupExampleInput2" placeholder="Date and Time of Delivery">
+                        <input type="datetime-local" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Date and Time of Delivery">
                       </div>
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Type of Delivery</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Type of Delivery">
+                        <input type="text" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Type of Delivery">
                       </div>
 
                       <div class="mb-3">
                         <label class="visually-hidden" for="inlineFormSelectPref"></label>
-                      <select class="form-select" id="inlineFormSelectPref">
+                      <select class="form-select border border-success" id="inlineFormSelectPref">
                         <option selected>Delivery Status</option>
                         <option value="1">Stillbirth</option>
                         <option value="2">Livebirth</option>
@@ -72,7 +72,7 @@
 
                       <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Condition at Birth</label>
-                        <input type="datetime" class="form-control" id="formGroupExampleInput2" placeholder="Condition at birth">
+                        <input type="datetime" class="form-control border border-success" id="formGroupExampleInput2" placeholder="Condition at birth">
                       </div>
 
                       <div class="text-end mt-3">
